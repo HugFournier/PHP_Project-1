8faa10e87b16dc5cf696ce75edfdfdb8759905e9
Damien Nguyen <damsng63@gmail.com>
2017-12-06 15:09:44 +0100

    CSS liste news

--- vues/vueNews.php
+++ vues/vueNews.php
@@ -8,51 +8,51 @@ global $front;
 <head>
     <meta charset="utf-8">
     <title>News</title>
-    <link rel="stylesheet" href="<?php echo $front['style']?>">
-    <link rel="stylesheet" href="<?php echo $front['bootstrap']?>">
+    <link rel="stylesheet" href="<?php echo $front['style'] ?>">
+    <link rel="stylesheet" href="<?php echo $front['bootstrap'] ?>">
 </head>
 
 <body>
-<center>
-<?php
-/**
- * Created by PhpStorm.
- * User: hufournier
- * Date: 22/11/17
- * Time: 14:46
- */
-global $rep;
-if (!isset($bdNews) || empty($bdNews)) {
-    echo "Aucune news à afficher<br>";
-}
-else{
+<div class="container">
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: hufournier
+     * Date: 22/11/17
+     * Time: 14:46
+     */
+    global $rep;
+    if (!isset($bdNews) || empty($bdNews)) {
+        echo "Aucune news à afficher<br>";
+    }
+    else{
     ?>
     <h1>Liste des News</h1>
-    <table class="table-bordered">
+    <table class="table-bordered full-width">
         <tr>
-            <th>Date</th>
-            <th>Nom</th>
-            <th>Description</th>
+            <th name="date">Date</th>
+            <th name="nom">Nom</th>
+            <th name="description">Description</th>
         </tr>
-    <?php
-    foreach ($bdNews as $news) {
-        echo "<tr><td>" . $news->getDateNews()."</td>";
-        echo "<td><a href=https://".$news->getLien().">" . $news->getTitre()."</a></td>";
-        echo "<td>" . $news->getDescription()."</td></tr>";
-    }
-}
-?>
+        <?php
+        foreach ($bdNews as $news) {
+            echo "<tr><td>" . $news->getDateNews() . "</td>";
+            echo "<td><a href=https://" . $news->getLien() . ">" . $news->getTitre() . "</a></td>";
+            echo "<td>" . $news->getDescription() . "</td></tr>";
+        }
+        }
+        ?>
     </table>
     <?php
-echo "<br><a href=?action=listerNews&page=1>1</a>|";
-echo "<a href=?action=listerNews&page=".($currentPage-1)."><</a>|";
-echo "[".$currentPage."]|";
-echo "<a href=?action=listerNews&page=".($currentPage+1).">></a>|";
-echo "<a href=?action=listerNews&page=".$nbPage.">".$nbPage."</a>";
-?>
+    echo "<br><a href=?action=listerNews&page=1>1</a>|";
+    echo "<a href=?action=listerNews&page=" . ($currentPage - 1) . "><</a>|";
+    echo "[" . $currentPage . "]|";
+    echo "<a href=?action=listerNews&page=" . ($currentPage + 1) . ">></a>|";
+    echo "<a href=?action=listerNews&page=" . $nbPage . ">" . $nbPage . "</a>";
+    ?>
     <br><br>
     <a href="?action=connectionAdmin">Connection</a>
-</center>
+</div>
 </body>
 
 </html>
