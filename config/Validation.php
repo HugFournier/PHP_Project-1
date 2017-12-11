2015387f7b1c7fb2c49b4613309eef040e644a48
Damien Nguyen <damsng63@gmail.com>
2017-12-11 16:33:42 +0100

    Fix validation

--- config/Validation.php
+++ config/Validation.php
@@ -4,23 +4,37 @@ class Validation
 {
     static function val_string(string $mot)
     {
-        return isset($mot) && $mot != "" && $mot==filter_var($mot, FILTER_SANITIZE_STRING);
+        return
+            isset($mot) &&
+            $mot != "" &&
+            $mot == filter_var($mot, FILTER_SANITIZE_STRING);
     }
 
-    static function val_page($current, $nbPage){
-        if(Validation::val_int($current)){
-            if($current<1)return 1;
-            if($current>$nbPage)return $nbPage;
+    static function val_page($current, $nbPage)
+    {
+        if (Validation::val_int($current)) {
+            if ($current < 1) return 1;
+            if ($current > $nbPage) return $nbPage;
             return $current;
-        }
-        else {
+        } else {
             return 1;
         }
     }
 
-    static function val_int($entier){
-        return isset($entier) && $entier!= "" && filter_var($entier, FILTER_VALIDATE_INT);
+    static function val_int($entier)
+    {
+        return
+            isset($entier) &&
+            $entier != ""&&
+            filter_var($entier, FILTER_SANITIZE_NUMBER_INT) &&
+            filter_var($entier, FILTER_VALIDATE_INT);
+    }
+
+    static function val_url($url)
+    {
+        return
+            isset($url) &&
+            $url != "" &&
+            filter_var($url, FILTER_SANITIZE_URL);
     }
 }
-
-?>
