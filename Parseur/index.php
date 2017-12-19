154f9dcdcd9485f075f66aa6cb3c397ff01a0d96
Damien Nguyen <damsng63@gmail.com>
2017-12-19 18:21:23 +0100

    Mini fix

--- Parseur/index.php
+++ Parseur/index.php
@@ -26,21 +26,34 @@
 
         $listeNews = $parser->getListeNews();
         foreach ($listeNews as $news) {
-            $news["pubdate"] = date("Y-m-d H:i:s", strtotime($dateNews));
+            if (isset($news["pubdate"])) {
+                $news["pubdate"] = date("Y-m-d H:i:s", strtotime($news["pubdate"]));
+            } else {
+                $news["pubdate"] = date('Y-m-d H:i:s');
+            }
+
+            if (!isset($news["guid"])) {
+                $news["guid"] = $news["link"];
+            }
+
+            if (!isset($news["description"])) {
+                $news["description"] = "Aucune description";
+            }
+
             try {
                 $query    = "INSERT INTO NEWS VALUES(:id, :dateNews, :description, :titre, :lien, :origine)";
                 $argument = array(
                     ':id' => array($news["guid"], PDO::PARAM_STR),
-                    ':dateNews' => array(NULL, PDO::PARAM_STR),
+                    ':dateNews' => array($news["pubdate"], PDO::PARAM_STR),
                     ':description' => array($news["description"], PDO::PARAM_STR),
                     ':titre' => array($news["title"], PDO::PARAM_STR),
                     ':lien' => array($news["link"], PDO::PARAM_STR),
                     ':origine' => array($news["origine"], PDO::PARAM_STR)
                 );
                 $con->executeQuery($query, $argument);
-                echo "News ajoutée";
+                echo "News ajoutée"  . "<br>";
             } catch (Exception $e) {
-                echo $e->getMessage();
+                echo $e->getMessage() . "<br>";
             }
         }
     }
